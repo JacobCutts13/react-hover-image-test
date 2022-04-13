483342e24ea3fe450669c77263541b6a32d02c9f
jacobcutts13 <jacobdcutts@gmail.com>
2022-04-13 17:28:43 +0100

    only fades image

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -9,12 +9,12 @@ interface IProps {
 }
 
 export default function Movie(Props: IProps): JSX.Element {
-  const opacityPerm = Props.hoverID === Props.movie.id? 0.8: 1;
+  const opacityPerm = Props.hoverID === Props.movie.id? 0.6: 1;
     return (
   <>    
     <motion.div
     layout
-    animate={{ opacity: opacityPerm }}
+    animate={{ opacity: 1 }}
     initial={{ opacity: 0 }}
     exit={{ opacity: 0 }}
     transition={{ duration: 0.5 }}
@@ -23,19 +23,22 @@ export default function Movie(Props: IProps): JSX.Element {
     >
     <h2>{Props.movie.title}</h2>
     <div className="movie-img" id={Props.movie.id.toString()+"img"}>
-            <img
+        <motion.div animate={{ opacity: opacityPerm }} >
+        <img
                 src={"https://image.tmdb.org/t/p/w500" + Props.movie.backdrop_path}
                 alt=""
                 id={Props.movie.id.toString()}
                 onMouseOver={e => (Props.setHoverID(parseInt(e.currentTarget.id)))}
                 onMouseOut={() => Props.setHoverID(0)}
             />
-            {Props.hoverID === Props.movie.id &&
-            <>
-                <div className="top-left">{Props.movie.title}</div>
-                <div className="bottom-left">{Props.movie.title}</div>
-            </>
-            } 
+        </motion.div>   
+
+        {Props.hoverID === Props.movie.id &&
+        <>
+            <div className="top-left">{Props.movie.title}</div>
+            <div className="bottom-left">{Props.movie.title}</div>
+        </>
+        } 
     </div>
     </motion.div>    
   </>
